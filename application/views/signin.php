fec0e5ee50d00459bae83b2c85ce1dbea17da8f2
mukko <mukk0.aquarius@gmail.com>
2015-08-14 16:25:45 +0900

    入力フォームを書き直し

--- application/views/signin.php
+++ application/views/signin.php
@@ -6,35 +6,35 @@
     <title>sign in</title>
 </head>
 <body>
-<form>
-    <fieldset>
-        <table>
-            <tbody>
-            <tr>
-                <th>userID</th>
-                <td><input type="text" id="userID" name="userID" value="" /></td>
-            </tr>
-            <tr>
-                <th>password</th>
-                <td><input type="text" id="password" name="password" value="" /></td>
-            </tr>
-            <tr>
-                <th>password (確認)</th>
-                <td><input type="text" id="check_password" name="check_password" value="" /></td>
-            </tr>
-            <tr>
-                <th>mail</th>
-                <td><input type="text" id="mail" name="mail" value="" /></td>
-            </tr>
-            <tr>
-                <th>mail (確認)</th>
-                <td><input type="text" id="check_mail" name="check_mail" value="" /></td>
-            </tr>
-            </tbody>
-        </table>
-    </fieldset>
-<!--  submitしたら、メールを送った画面に遷移させたい  -->
-    <p class="submit"><input type="submit" name="submit" value="login" /></p>
-</form>
+
+<div id="container">
+    <h1>sign in</h1>
+
+    <?php
+
+    echo validation_errors();
+    echo form_open("signin/signin_validation");
+
+    echo "<p>userID: ";
+    echo form_input("userID");
+    echo "</p>";
+
+    echo "<p>Email: ";
+    echo form_input("email");
+    echo "</p>";
+
+    echo "<p>password: ";
+    echo form_password("password");
+    echo "</p>";
+
+    echo "<p>";
+    echo form_submit("signin_submit", "signin");
+    echo "</p>";
+
+    echo form_close();
+
+    ?>
+
+</div>
 </body>
 </html>
