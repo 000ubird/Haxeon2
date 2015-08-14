3e69224836f8875795cb56d3790cd32c44d30f34
Torimura Takanobu <000ubird@gmail.com>
2015-08-14 10:46:47 +0900

    共通部分のPHPファイルをCIに移植

---
+++ application/controllers/Common.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Common extends CI_Controller {
+	
+	public function index() {
+		$url['commonURL'] = "http://localhost/haxeon2/index.php/common/index/";
+		
+		//ログイン時はアイコンURLを取得する
+		if (isset($_COOKIE["PHPSESSID"])){
+			//セッションがアクティブでない場合はセッションを起動する
+			if (!(session_status() == PHP_SESSION_ACTIVE)) {
+				session_start();
+			}
+			
+			$id['id'] = $_SESSION['userID']; 
+			$flag['isLogin'] = false;
+			
+			$query = $this->db->get_where('account', array('userID' => $_SESSION['userID']));
+			
+			foreach ($query->result() as $row) {
+				$icon['iconURL'] = $row->userIcon;
+				$name['userName']= $row->userName;
+			}
+			$this->load->view('common', $flag, $url, $id, $icon, $name);
+		}
+		else {
+			$flag['isLogin'] = false; 
+			$this->load->view('common', $flag, $url);
+		}
+	}
+}
---
+++ application/views/common.php
@@ -0,0 +1,62 @@
+<html>
+	
+<link href="../../css/common.css" type="text/css" rel="stylesheet" />
+
+<!-- ヘッダー -->
+<div id="header"><h1><a href=""><img src="../../img/haxeon_icon.png" style="width:auto; height:75px;"></a></h1></div>
+
+<!-- タブリスト　-->
+<div class="menu">
+	<ul>
+		<!-- ログイン時はアカウント名とサービスを表示　-->
+		<?php if($isLogin) { ?>
+		<li><h3>Hello, <img src=<?php echo $iconURL ?> width=35px height=35px> {$userName}</h3>
+			<ul>
+				<li><a href=<?phpecho $commonURL?>profile.php?id={$id}>Profile</a></li>
+				<li><a href="#">Posted Codes</a></li>
+				<li><a href="#">Favorite Codes</a></li>
+				<li><a href=<?phpecho$commonURL?>followlist.php>Followers</a></li>
+				<li><a href=<?phpecho$commonURL?>logout.php>Logout</a></li>
+			</ul>
+		</li>
+		<!-- 未ログイン時はログインとサインアップのリンクを表示　-->
+		<?php } else { ?>
+		<li>Accounts
+			<ul>
+				<li><a href=<?php $commonURL?>>Login</a></li>
+				<li><a href=<?php $commonURL?>>SignUp</li>
+			</ul>
+		</li>
+		<?php } ?>
+		<li>Ranking
+			<ul>
+				<li><a href="#">Page View Ranking</a></li>
+				<li><a href="#">Favorite Ranking</a></li>
+				<li><a href="#">Forked Count Ranking</a></li>
+			</ul>
+		</li>
+
+		<li>Code
+			<ul>
+				<li><a href={$commonURL}create_form.php>Create Project</a></li>
+			</ul>
+		</li>
+
+		<li>Q＆A
+			<ul>
+				<li><a href="#">New Question</a></li>
+				<li><a href="#">Hot Questions</a></li>
+			</ul>
+		</li>
+
+		<li>About Haxe
+			<ul>
+				<li><a href="http://api.haxe.org/">Api</a></li>
+				<li><a href="https://github.com/HaxeFoundation/haxe">Github</a></li>
+				<li><a href="http://sipo.jp/blog/2014/01/25.html">enumのすごさ</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+</html>
