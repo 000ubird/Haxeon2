2541be8ff6cfa4def30c4a858ed8c1c1f4d8cb94
mukko <mukk0.aquarius@gmail.com>
2015-08-14 15:07:09 +0900

    ログイン画面のコントローラーを作成

---
+++ application/views/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>login</title>
+</head>
+<body>
+<form>
+<fieldset>
+<table>
+    <tbody>
+        <tr>
+            <th>userID</th>
+            <td><input type="text" id="userID" name="userID" value="" /></td>
+        </tr>
+        <tr>
+            <th>password</th>
+            <td><input type="text" id="password" name="password" value="" /></td>
+        </tr>
+    </tbody>
+</table>
+</fieldset>
+    <p class="submit"><input type="submit" name="submit" value="login" /></p>
+</form>
+</body>
+</html>
