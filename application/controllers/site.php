5562216b25bcb0cbe6b24549e44c624df82126cd
Torimura Takanobu <000ubird@gmail.com>
2015-08-19 11:56:10 +0900

    仮トップページを表示するコントローラーを追加

---
+++ application/controllers/site.php
@@ -0,0 +1,29 @@
+<?php
+class Site extends CI_Controller {
+
+	public function index() {
+		$this->top();
+	}
+	
+	//トップページの表示
+	public function top() {
+		$flag['isLogin'] = false;
+		$icon = "";
+		if (isset($_SESSION['userID'])) {
+			$flag['isLogin'] = true;
+			$this->load->model("model_users");
+			
+			echo $icon['url'] = $this->model_users->get_icon_url($_SESSION['userID']);
+			
+			$this->load->view("header",array($flag,$icon));
+		}
+		else {
+			$this->load->view("header",$flag);
+		}
+		
+		$this->load->view("haxeon2");
+		$this->load->view("footer");
+	}
+}
+
+?>
