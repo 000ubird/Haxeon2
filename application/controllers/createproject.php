9ec0b42f7ce77c488d74e3537d015840210ddbb7
mukko <mukk0.aquarius@gmail.com>
2015-08-19 12:45:46 +0900

    [WIP]処理の流れを記した

--- application/controllers/createproject.php
+++ application/controllers/createproject.php
@@ -13,6 +13,19 @@ class Createproject extends CI_Controller {
     }
 
     public function create_validation(){
+        //フォームバリデーションライブラリを呼び出し
+        $this->load->library("form_validation");
 
+        $this->form_validation->set_error_delimiters('<div class="error">','</div>');
+
+        //入力の有無確認
+        $this->form_validation->set_rules("projectName", "プロジェクト名", "require");
+        $this->form_validation->set_message("required", "%sが入力されていません");
+
+        //入力されたプロジェクト名がすでに登録されてないかを確認
+        //モデルに問い合わせる
+        //$this->form_validation->set_message("required", "すでに登録されているプロジェクト名です");
+        //登録、try-haxeへ
+        //リダイレクト
     }
 }
