048379369c949417a44cb7a7e00ed6b0bcc5bc37
Torimura Takanobu <000ubird@gmail.com>
2015-08-21 12:36:00 +0900

    modelにサインアップに関するメソッドを定義
    ・一時登録用のテーブルにアカウント情報を登録するメソッドを定義
    ・一時登録アカウントを本登録テーブルに追加するメソッドを定義
    ・その他、アカウント情報の重複チェックメソッドを修正

--- application/models/model_users.php
+++ application/models/model_users.php
@@ -4,25 +4,16 @@ class Model_users extends CI_Model{
 
     //DBにユーザーデータがあったらtrueを返す
     public function can_log_in(){
-        $this->db->where(array('userID' => $this->input->post("userID"), 'userPass' => $this->input->post("password")));
+        $this->db->where(array('userID' => $this->input->post('userID'), 'userPass' => $this->input->post('password')));
         $query = $this->db->get('account');
 
         return ($query->num_rows() == 1);
     }
 
-	//ユーザIDからアイコンのあるURLを取得
-    public function is_overlap_uid($userID)
-    {
-        $this->db->where(array('userID' => $userID));
-        $query = $this->db->get('account');
-
-        //重複していた場合は真
-        return ($query->num_rows() == 1);
-    }
-
-    public function get_icon_url($userID){
-		$icon = "";
-
+	//ユーザIDからアイコンのあるURLを取得	
+	public function get_icon_url($userID) {
+		$icon = '';
+		
 		$query = $this->db->get_where('account', array('userID' => $userID));
 		foreach ($query->result() as $row) {
 			$icon = $row->userIcon;
@@ -82,4 +73,55 @@ class Model_users extends CI_Model{
 
         return $query->result();
     }
+	
+	//一時登録用テーブルにユーザー情報を追加する
+	public function add_tmp_user($key) {
+		$data = array(
+			'userID' => $this->input->post('userID'),
+			'password' => $this->input->post('password'),	//暗号化必要?
+			'mail' => $this->input->post('email'),
+			'registKey'=> $key
+		);
+		
+		return $this->db->insert('tmp_account',$data);
+	}
+	
+	//一時登録用テーブルにユーザー情報を追加する
+	public function add_user($key) {
+		$this->db->where('registKey', $key);	//キーからユーザー情報を取得
+		$data = $this->db->get('tmp_account');
+		
+		if ($data) {
+			$row = $data->row();
+			$info = array(
+				'userID' => $row->userID,
+				'userPass' => $row->password,
+				'userName' => $row->userID,
+				'userMail' => $row->mail,
+			);
+			if ($this->db->insert("account", $info)) {
+				echo "アカウントが有効になりました!";
+			}
+		}
+	}
+	
+	//ユーザIDの重複チェック
+	public function is_overlap_uid($userID) {
+		$this->db->where(array('userID' => $userID));
+		$query1 = $this->db->get('account');
+		$query2 = $this->db->get('tmp_account');	//一時登録用のテーブルも確認
+		
+		//重複していた場合は真
+		return ($query1->num_rows() > 0 || $query2->num_rows() > 0);
+	}
+	
+	//メールアドレスの重複チェック
+	public function is_overlap_mail($mail) {
+		$this->db->where(array('userMail' => $mail));
+		$query1 = $this->db->get('account');
+		$query2 = $this->db->get('tmp_account');
+		
+		//重複していた場合は真
+		return ($query1->num_rows() > 0 || $query2->num_rows() > 0);
+	}
 }
