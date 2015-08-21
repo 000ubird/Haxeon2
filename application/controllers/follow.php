26a6d882c44f3b65e7728419530118e648390bb2
mukko <mukk0.aquarius@gmail.com>
2015-08-21 16:37:20 +0900

    フォローしたらデータベースに登録する処理を作成

--- application/controllers/follow.php
+++ application/controllers/follow.php
@@ -4,8 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Follow extends CI_Controller {
 
     public function index(){
-        //とりあえずページを表示
-        $this->load->view();
+
+    }
+
+    //引数に入るフォロー相手のuserIDと、セッションにある自分のIDを使う
+    public function accountFollow($data){
+        //DBにinsertする
+        $this->load->model('model_follow');
+        $this->model_follow->apply($data);
+
+        $this->load->view('haxeon2');
     }
 
 }
--- application/models/model_follow.php
+++ application/models/model_follow.php
@@ -2,4 +2,18 @@
 
 class model_follow extends CI_Model {
 
+    //$userID フォロー相手のuserID
+    //フォローをする
+    public function apply($userID){
+        //データの作成
+        $data = array(
+            'userID' => $this->session->userdata('userID'),
+            'userFollowingID' => $userID
+        );
+
+        //insert
+        $this->db->insert('follow', $data);
+
+    }
+
 }
