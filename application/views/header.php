73c3068d46a2ae36b0b9d0e546c386c247b70bfb
mukko <mukk0.aquarius@gmail.com>
2015-08-24 11:39:35 +0900

    共通のヘッダを作りなおした
    view(header)を読みこめばok

--- application/views/header.php
+++ application/views/header.php
@@ -1,23 +1,43 @@
-<!-- <!DOCTYPE html> -->
-<link rel="stylesheet" href="../css/header.css" type="text/css" />
-
-<html lang="ja">
+<html>
 
 <head>
-	<a href=<?php echo base_url();?>>haxeon</a>
-	<a href=<?php echo base_url();?>createproject>Create Project</a>
-	<a href=<?php echo base_url();?>question>Questions</a>
-	<a href=<?php echo base_url();?>ranking>Ranking</a>
+<link type="text/css" rel="stylesheet" href="<?php echo base_url()?>css/materialize.min.css"  media="screen,projection"/>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-	<?php if($this->session->userdata('userID') != "") { ?>
-		ログイン済みです。
-		<a href=<?php echo base_url();?>profile/information/<?php echo $this->session->userdata('userID'); ?>>Account</a>
-        <a href=<?php echo base_url();?>logout>Logout</a>
-        <a href=<?php echo base_url();?>profile/infomation/<?php echo $this->session->userdata('userID');?>><img src=<?php echo "icon_URL"?></a>
-	<?php } else { ?>
-		未ログイン状態です。
-		<a href=<?php echo base_url();?>login>Login</a>
-		<a href=<?php echo base_url();?>signup>Signup</a>
-	<?php } ?>
-	<a href=<?php echo base_url();?>about>About</a>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url()?>js/materialize.min.js"></script>
+
+<!--  追加したjs。あとで別ファイルにしてsrcで読み込む  -->
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".button-collapse").sideNav();
+    });
+</script>
+
+    <title>haxeon</title>
 </head>
+
+<body>
+<header>
+    <nav>
+        <div class="nav-wrapper amber accent-4">
+            <a href="<?php echo base_url() ?>" class="brand-logo">Haxeon</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+
+            <!--     PCサイズでの表示       -->
+            <ul class="right hide-on-med-and-down">
+                <li><a href="createproject">Create Code</a></li>
+                <li><a href="">Ranking</a></li>
+                <li><a href="profile">Profile</a></li>
+            </ul>
+            <!--      スマホサイズでの表示      -->
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="createproject">Create Code</a></li>
+                <li><a href="">Ranking</a></li>
+                <li><a href="profile">Profile</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
