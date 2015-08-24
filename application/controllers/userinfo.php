44a54cddff2a3a1521f2e1a8135d55f408da7af1
Torimura Takanobu <000ubird@gmail.com>
2015-08-24 16:48:35 +0900

    UserinfoクラスにPV数を増やす処理を行うメソッドを定義

--- application/controllers/userinfo.php
+++ application/controllers/userinfo.php
@@ -3,20 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Userinfo extends CI_Controller {
 
-    public function index(){
-        if(($this->session->userdata('userID') == null))
-        {
-            echo '{ "error":"not login"}';
-            exit;
-        }
-
-        if(($this->session->userdata('projectName') == null))
-        {
-            echo '{"userID":"'.$this->session->userdata('userID').'","projectName":""}';
-            exit;
-        }
-
-
-        echo '{"userID":"'.$this->session->userdata('userID').'","projectName":"'.$this->session->userdata('projectName').'"}';
-      }
+	public function index() {
+		if(($this->session->userdata('userID') == null)) {
+			echo '{ "error":"not login"}';
+			exit;
+		}
+	
+		if(($this->session->userdata('projectName') == null)) {
+			echo '{"userID":"'.$this->session->userdata('userID').'","projectName":""}';
+			exit;
+		}
+		
+		echo '{"userID":"'.$this->session->userdata('userID').'","projectName":"'.$this->session->userdata('projectName').'"}';
+	}
+	
+	//http://localhost/haxeon2/userinfo/update_pv/Tom0/HelloWorld_tom0
+	public function update_pv($uid, $projectName) {
+		$this->load->model('model_project');
+		$this->model_project->pvCountUp($uid,$projectName);
+	}
 }
