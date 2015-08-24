3931b10b224bd8e25020b2a7dc9b4d2dfac64ea9
mukko <mukk0.aquarius@gmail.com>
2015-08-24 12:38:49 +0900

    signupページを修正

--- application/views/signup.php
+++ application/views/signup.php
@@ -1,41 +1,30 @@
-<!DOCTYPE html>
+<h1>アカウント登録</h1>
 
-<link href="../css/convenience.css" type="text/css" rel="stylesheet">
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>sign up</title>
-</head>
-<body>
+<?php
 
-<div id="container">
-    <h1>アカウント登録</h1>
+echo form_open("signup/validation");
 
-    <?php
+echo "<p>userID: ";
+echo form_input("userID", set_value('userID'));
+echo " 【必須】 </p>";
+echo form_error('userID');
 
-    echo form_open("signup/validation");
-	
-	echo "<p>userID: ";
-    echo form_input("userID", set_value('userID'));
-    echo " 【必須】 </p>";
-	echo form_error('userID');
-	
-    echo "<p>password: ";
-    echo form_password("password", set_value('password'));
-    echo " 【必須】 </p>";
-	echo form_error('password');
-	
-    echo "<p>Email  : ";
-    echo form_input("email", set_value('email'));
-    echo " 【必須】 </p>";
-	echo form_error('email');
-	
-	//送信ボタン
-    echo form_submit("signin_submit", "送信");
+echo "<p>password: ";
+echo form_password("password", set_value('password'));
+echo " 【必須】 </p>";
+echo form_error('password');
 
-    echo form_close();
-    ?>
+echo "<p>Email  : ";
+echo form_input("email", set_value('email'));
+echo " 【必須】 </p>";
+echo form_error('email');
 
-</div>
-</body>
-</html>
+?>
+
+<button class="btn waves-effect waves-light amber accent-4" type="submit" name="action">Login
+    <i class="material-icons">input</i>
+</button>
+
+<?php
+echo form_close();
+?>
