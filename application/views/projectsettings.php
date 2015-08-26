1c6b2b866049d370af3a9ebdc28e6644e857fed9
mukko <mukk0.aquarius@gmail.com>
2015-08-26 18:15:57 +0900

    タグ登録のための入力フォームを作成

--- application/views/projectsettings.php
+++ application/views/projectsettings.php
@@ -6,4 +6,22 @@ foreach ($tags as $tag) {
     echo '<p>'. $tag .'</p>';
 }
 
+echo form_open('profile/validation');
+
+echo validation_errors();
+
+echo '<div class="row">';
+echo '<div class="input-field col s12">';
+echo '<input name="tag" type="text" class="validate">';
+echo '<label for="tag">new tag</label>';
+echo '</div>';
+echo '</div>';
+
+echo '<div class="row">';
+echo '<button class="btn waves-effect waves-light orange darken-4" style="float:right" type="submit" name="action">create Tag ';
+echo '<i class="material-icons">input</i>';
+echo '</button>';
+echo '</div>';
+
+echo form_close();
 ?>
