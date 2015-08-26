79c53c761ceeca914687b5014584c7c48b903479
mukko <mukk0.aquarius@gmail.com>
2015-08-26 18:16:22 +0900

    タグ登録のバリデーションを作成

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -38,9 +38,11 @@ class Profile extends CI_Controller {
      * タグ設定やプロジェクトの削除など
      */
     public function projectsettings($projectID){
+        $this->session->set_userdata(array('pid' => $projectID));
+
         $this->load->model('model_project');
         $this->load->library('tag');
-        
+
         //sessionのuserIDとprojectIDの所有者が同じかチェック
         if($this->model_project->isOwner($projectID)) {
 
@@ -51,6 +53,36 @@ class Profile extends CI_Controller {
             $this->load->view('footer');
         }else{
             $this->index();
+        }
+    }
+
+    public function validation(){
+        $this->load->library("form_validation");
+        $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+
+        //検証ルールの設定
+        $this->form_validation->set_rules("tag", "タグ", "required|callback_tag_check");
+
+        $this->form_validation->set_message("required", "%s を入力してください");
+
+        //正しい場合は登録処理
+        if ($this->form_validation->run()) {
+
+        }else{
+            $this->projectsettings($this->session->userdata('pid'));
+        }
+    }
+
+    //タグの重複チェック
+    public function tag_check($str) {
+        $this->load->model("model_project");
+
+        if($this->model_project->isTag($str)){
+            $this->form_validation->set_message('tag_check','入力された %s '.$str.' は既に使われております。');
+            return false;
+        }
+        else {
+            return true;
         }
     }
 }
