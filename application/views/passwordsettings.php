b3601912a673643adb182c9cd7936dac1f0cc8bd
mukko <mukk0.aquarius@gmail.com>
2015-09-02 15:12:43 +0900

    phpを必要なところのみにした

--- application/views/passwordsettings.php
+++ application/views/passwordsettings.php
@@ -1,38 +1,35 @@
 <h1>パスワード変更確認</h1>
 
-<?php
+<?php echo form_open('profile/validation_password/'.$userID); ?>
 
-echo form_open();
+<div class="row">
+<div class="input-field col s12">
+<input name="current" type="password">
+<label for="current">現在のパスワード</label>
+</div>
+<?php echo form_error('current'); ?>
+</div>
 
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="current" type="text">';
-echo '<label for="current">現在のパスワード</label>';
-echo '</div>';
-echo form_error('current');
-echo '</div>';
+<div class="row">
+<div class="input-field col s12">
+<input name="new" type="password">
+<label for="new">新しいパスワード</label>
+</div>
+<?php echo form_error('new'); ?>
+</div>
 
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="new" type="text">';
-echo '<label for="new">新しいパスワード</label>';
-echo '</div>';
-echo form_error('new');
-echo '</div>';
+<div class="row">
+<div class="input-field col s12">
+<input name="again" type="password">
+<label for="again">新しいパスワード(再入力)</label>
+</div>
+<?php echo form_error('again'); ?>
+</div>
 
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="again" type="text">';
-echo '<label for="again">新しいパスワード(再入力)</label>';
-echo '</div>';
-echo form_error('again');
-echo '</div>';
+<div class="row">
+<button class="btn waves-effect waves-light orange darken-4 col s3 offset-s9" type="submit" name="action">change
+<i class="material-icons">open_in_new</i>
+</button>
+</div>
 
-echo '<div class="row">';
-echo '<button class="btn waves-effect waves-light orange darken-4 col s3 offset-s9" type="submit" name="action">change';
-echo '<i class="material-icons">open_in_new</i>';
-echo '</button>';
-echo '</div>';
-
-echo form_close();
-?>
+<?php echo form_close(); ?>
