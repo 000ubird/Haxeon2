087a3240c451a78becc4decf81c9a9469fc6047e
mukko <mukk0.aquarius@gmail.com>
2015-09-02 11:37:10 +0900

    プロフイール設定画面をタブ化

--- application/views/profilesettings.php
+++ application/views/profilesettings.php
@@ -1,54 +1,63 @@
 <h2>プロフィール設定</h2>
 
-<?php
-echo $error;
-//ファイルアップロードフォーム
-echo form_open_multipart('profile/do_upload/'.$userID);
-//アイコン
-echo '<input type="file" name="userfile" size="20" />';
-echo '<input type="submit" value="upload" />';
-echo form_close();
+<div class="row">
+    <div class="col s12 orange darken-4">
+        <ul class="tabs">
+            <li class="tab col s6"><a class="active" href="#common">情報</a></li>
+            <li class="tab col s6"><a href="#important">重要</a></li>
+        </ul>
+    </div>
+</div>
 
-echo form_open('profile/validation_profile/'.$userID);
+<div id="common" class="col s12">
+    <?php
+    echo form_open_multipart('profile/do_upload/'.$userID);
+    //アイコン
+    echo '<input type="file" name="userfile" size="20" />';
+    echo '<input type="submit" value="upload" />';
+    echo form_close();
 
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="userName" type="text">';
-echo '<label for="userName">userName</label>';
-echo '</div>';
-echo form_error('userName');
-echo '</div>';
+    echo form_open('profile/validation_profile/'.$userID);
 
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="password" type="password">';
-echo '<label for="password">password</label>';
-echo '</div>';
-echo form_error('password');
-echo '</div>';
+    echo '<div class="row">';
+    echo '<div class="input-field col s12">';
+    echo '<input name="userName" type="text">';
+    echo '<label for="userName">userName</label>';
+    echo '</div>';
+    echo form_error('userName');
+    echo '</div>';
 
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="email" type="email">';
-echo '<label for="email">email</label>';
-echo '</div>';
-echo form_error('email');
-echo '</div>';
+    //メッセージ文
+    echo '<div class="row">';
+    echo '<div class="input-field col s12">';
+    echo '<input name="profile" type="text">';
+    echo '<label for="profile">message</label>';
+    echo '</div>';
+    echo form_error('profile');
+    echo '</div>';
 
-//メッセージ文
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="profile" type="text">';
-echo '<label for="profile">message</label>';
-echo '</div>';
-echo form_error('profile');
-echo '</div>';
+    echo '<div class="row">';
+    echo '<button class="btn waves-effect waves-light orange darken-4 col s3 offset-s9" type="submit" name="action">Change';
+    echo '<i class="material-icons">open_in_new</i>';
+    echo '</button>';
+    echo '</div>';
 
-echo '<div class="row">';
-echo '<button class="btn waves-effect waves-light orange darken-4 col s3 offset-s9" type="submit" name="action">Change';
-echo '<i class="material-icons">open_in_new</i>';
-echo '</button>';
-echo '</div>';
+    echo form_close();
+    ?>
+</div>
 
-echo form_close();
-?>
+<div id="important" class="col s12">
+    <?php
+    echo '<div class="row">';
+    echo '<button class="btn waves-effect waves-light orange darken-4 col s4 offset-s4" value="パスワードを変更する" onClick="window.open(\'https://www.google.co.jp\')">パスワードを変更する';
+    echo '<i class="material-icons">open_in_new</i>';
+    echo '</button>';
+    echo '</div>';
+
+    echo '<div class="row">';
+    echo '<button class="btn waves-effect waves-light orange darken-4 col s4 offset-s4" value="メールアドレスを変更する" onClick="window.open(\'https://www.google.co.jp\')">メールアドレスを変更する';
+    echo '<i class="material-icons">open_in_new</i>';
+    echo '</button>';
+    echo '</div>';
+    ?>
+</div>
