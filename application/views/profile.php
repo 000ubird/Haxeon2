779c7d739799cffd80af6d8da22630c7aec76c9d
mukko <mukk0.aquarius@gmail.com>
2015-09-02 12:10:16 +0900

    アカウント削除ボタンを設定ページに移植

--- application/views/profile.php
+++ application/views/profile.php
@@ -101,5 +101,4 @@ echo '<div class="following">';
 echo '</div>';
 echo '</div>';
 
-echo '<a class="waves-effect waves-light red btn" href="http://localhost/haxeon2/profile/delete"><i class="material-icons left">delete</i>DELETE ACCOUNT</a>';
 ?>
