362dbad78f57dfef8f80610f754c56bcffb4ab3e
mukko <mukk0.aquarius@gmail.com>
2015-09-18 17:02:24 +0900

    fix createproject

--- application/views/createproject.php
+++ application/views/createproject.php
@@ -1,28 +1,25 @@
 <h1>新規プロジェクトページ</h1>
-<!--テスト表示-->
+
 <h2>セッション情報</h2>
 <?php
-
 print_r($this->session->all_userdata());
-//ここまで
 
 echo form_open("createproject/validation");
-
 echo validation_errors();
+?>
 
-echo '<div class="row">';
-echo '<div class="input-field col s12">';
-echo '<input name="projectName" type="text">';
-echo '<label for="projectName">projectName</label>';
-echo '</div>';
-echo '</div>';
+<div class="row">
+<div class="input-field col s12">
+<input name="projectName" type="text">
+<label for="projectName">projectName</label>
+</div>
+</div>
 
-echo '<div class="row">';
-echo '<button class="btn waves-effect waves-light orange darken-4" style="float:right" type="submit" name="action">Create ';
-echo '<i class="material-icons">note_add</i>';
-echo '</button>';
-echo '</div>';
+<div class="row">';
+<button class="btn waves-effect waves-light orange darken-4" style="float:right" type="submit" name="action">Create<i class="material-icons">note_add</i>
+</button>
+</div>
 
+<?php
 echo form_close();
-
 ?>
