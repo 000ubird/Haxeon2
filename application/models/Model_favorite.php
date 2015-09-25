55cac14e0c2f4fb5f308d52ef085c5adde7b1857
mukko <mukk0.aquarius@gmail.com>
2015-09-25 18:19:10 +0900

    favoriteのデータベース操作を作成
    create, delete, update

--- application/models/Model_favorite.php
+++ application/models/Model_favorite.php
@@ -4,16 +4,26 @@ class Model_favorite extends CI_Model {
 
     //favoriteテーブルにuserIDとprojectIDを登録する
     public function favorite($userID, $projectID){
+        $data = array(
+            'userID' => $userID,
+            'projectID' => $projectID
+        );
 
+        $this->db->insert('favorite', $data);
     }
 
     //favoriteテーブルからuserIDとprojectIDで検索したものを削除する
     public function release_favorite($userID, $projectID){
-
+        $this->db->delete('favorite', array('userID' => $userID, 'projectID' => $projectID));
     }
 
     //favoriteテーブルのprojectIDをアップデートする
     public function update_favorite($userID, $beforeID, $afterID){
-        
+        $data = array(
+            'userID' => $userID,
+            'projectID' => $beforeID
+        );
+        $this->db->where($data);
+        $this->db->update('favorite', array('projectID' => $afterID));
     }
 }
