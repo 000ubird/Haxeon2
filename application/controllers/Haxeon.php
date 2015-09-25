66483909f7ccff63121031fbaebb9e8879669dbe
Torimura Takanobu <000ubird@gmail.com>
2015-09-25 18:23:54 +0900

    トップページに簡易ランキングを表示する機能を追加

--- application/controllers/Haxeon.php
+++ application/controllers/Haxeon.php
@@ -4,8 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Haxeon extends CI_Controller {
 
 	public function index() {
+		$this->load->model('Model_project');
+		$projects = $this->Model_project->getRankingProject();
+		$data['ranking'] = array('projects'=>$projects);
+		
 		$this->load->view('header');
-		$this->load->view('haxeon');
+		$this->load->view('haxeon',$data);
 		$this->load->view('footer');
 	}
 }
--- application/views/haxeon.php
+++ application/views/haxeon.php
@@ -1,8 +1,50 @@
-<h1>セッション情報</h1>
+<div align="center">
+<img src="img/top.jpg" width="767" height="230" alt="">
+</div>
 
-    <?php
+<h2>Today's HotCode!</h2>
 
-    print_r($this->session->all_userdata());
+<?php
+$cur_page = 0;
+$projects = $ranking['projects'];
 
-    ?>
+//カードの表示
+echo '<div class="row">';
+$i = 0;
+foreach($projects as $project) {
+	echo '<div class="col s4">';
+	echo '<div class="card">';
 
+	//1,2,3位のプロジェクトには特別な画像を表示する
+	echo '<div class="card-image waves-effect waves-block waves-light">';
+		if ($i == 0 && $cur_page == 0) echo '<img class="activator" src="http://localhost/haxeon/img/1st_pro.jpg">';
+		else if ($i == 1 && $cur_page == 0) echo '<img class="activator" src="http://localhost/haxeon/img/2nd_pro.jpg">';
+		else if ($i == 2 && $cur_page == 0) echo '<img class="activator" src="http://localhost/haxeon/img/3rd_pro.jpg">';
+		else echo '<img class="activator" src="http://localhost/haxeon/img/project.jpg">';
+	echo '</div>';
+
+	echo '<div class="card-content">';
+	echo '<span class="card-title activator black-text text-darken-4">'.$project->proID.'<i class="material-icons right">more_vert</i></span>';
+	echo '<p>User : '.$project->usrID.'</p>';
+	echo '<p>pv : '.$project->pv.'</p>';
+	echo '<p><a href=http://localhost/haxeon/try-haxe/index.html#'.$project->proID.'>Edit Code</a></p>';
+	echo '</div>';
+
+	echo '<div class="card-reveal">';
+	echo '<span class="card-title grey-text text-darken-4">'.$project->proID.'<i class="material-icons right">close</i></span>';
+	echo '<p>Project ID : '.$project->proID.'</p>';
+	echo '<p>pv : '.$project->pv.'</p>';
+	echo '<p>User : '.$project->usrID.'</p>';
+	echo '</div>';
+
+	echo '</div>';
+	echo '</div>';
+	$i++;
+}
+echo '</div>';
+
+if($i==0) {
+	echo "<p>現在、ランキングに関する情報がありません。</p>";
+}
+
+?>
