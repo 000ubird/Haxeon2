7fa0686cee31f29b86dc5d45d45af0f1c82523eb
mukko <mukk0.aquarius@gmail.com>
2015-09-25 15:33:17 +0900

    登録、削除のメソッドの大枠を作成

--- application/models/Model_favorite.php
+++ application/models/Model_favorite.php
@@ -1,5 +1,14 @@
 <?php
 
 class Model_favorite extends CI_Model {
-    
+
+    //favoriteテーブルにuserIDとprojectIDを登録する
+    public function favorite($userID, $projectID){
+
+    }
+
+    //favoriteテーブルからuserIDとprojectIDで検索したものを削除する
+    public function release_favorite($userID, $projectID){
+        
+    }
 }
