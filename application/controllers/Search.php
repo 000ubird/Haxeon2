90b1f3d37d14c7bfe5504eb8d01dff2e47299c56
Torimura Takanobu <000ubird@gmail.com>
2015-10-23 17:57:13 +0900

    Controlerに検索機能を実装

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -1,7 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+define('NUM_CHECKBOX', 4);	//サーチ画面のチェックボックスの数
 
 class Search extends CI_Controller {
+
 	//検索フォームの表示
 	public function index() {
 		$this->load->view('header');
@@ -11,18 +13,29 @@ class Search extends CI_Controller {
 	
 	//検索を実行
 	public function doSearch() {
-		$this->load->view('header');
-		
 		//検索文字列の取得
 		$str = $this->input->get_post('search', TRUE);
-		echo $str."<br>";
 		
 		//チェックボックス情報の取得
-		if (set_checkbox('chk[0]', '1')) echo "TAG is checked <br>";
-		if (set_checkbox('chk[1]', '2')) echo "ProjectName is checked <br>";
-		if (set_checkbox('chk[2]', '3')) echo "ProjectID is checked <br>";
-		if (set_checkbox('chk[3]', '4')) echo "AccountID is checked <br>";
+		//0:tag, 1:projectName, 2:projectID, 3:accountID
+		$searchArray = [false, false, false, false];
+		for ($i = 0; $i < NUM_CHECKBOX; $i++ ) {
+			//チェックが付いていた所だけtrueを代入する
+			if (set_checkbox('chk['.$i.']', $i)) $searchArray[$i] = true;
+		}
 		
+		//データベースから検索
+		$this->load->model("Model_project");
+		$result = $this->Model_project->searchProject($str,$searchArray);
+		
+		//デバッグ
+		foreach($result as $row){
+			echo $row['projectID'].$row['ownerUserID']."<br>";
+		}
+		
+		//Viewを表示
+		$this->load->view('header');
+		//$this->load->view('search_result');
 		$this->load->view('footer');
 	}
 }
