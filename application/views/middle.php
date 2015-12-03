a95a011688492cbaca0518b0285debbd0f3ac582
mukko <mukk0.aquarius@gmail.com>
2015-12-03 11:18:38 +0900

    説明文のビューを作成。改行が反映されるようにnl2brを使用

--- application/views/middle.php
+++ application/views/middle.php
@@ -1,2 +1,12 @@
 <h1><?php echo $projectName ?></h1>
 <pre><code class="haxe"><?php echo $program; ?></code></pre>
+
+<div class="row description">
+    <div class="col s10 offset-s1">
+        <div class="card-panel white">
+            <span>
+                <?php echo nl2br($description); ?>
+            </span>
+        </div>
+    </div>
+</div>
