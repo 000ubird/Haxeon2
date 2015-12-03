cd657757834d52c17d43cd263db83e5915b25047
mukko <mukk0.aquarius@gmail.com>
2015-12-03 16:53:11 +0900

    CRD部分を作成

--- application/models/Model_comment.php
+++ application/models/Model_comment.php
@@ -4,17 +4,28 @@ class Model_comment extends CI_Model{
 
     //コメントを登録する
     public function registComment($projectID, $comment, $commentedUserID){
+        $array = array(
+            'projectID' => $projectID,
+            'comment' => $comment,
+            'commentedUserID' => $commentedUserID
+        );
 
+        $this->db->insert('comment', $array);
     }
 
     //コメントを削除する
     public function deleteComment($commentID){
+        $array = array(
+            'commentID' => $commentID
+        );
 
+        $this->db->delete('comment, $array');
     }
 
     //コメントを取得する
     //projectIDに紐づけられた全てのコメントを結果として返す
     public function getComment($projectID){
-
+        $query = $this->db->get_where('comment', array('projectID' => $projectID));
+        return $query->result();
     }
 }
