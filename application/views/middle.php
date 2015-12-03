9cfe292611daf8c995e0f37521416bbdd85c2e0b
mukko <mukk0.aquarius@gmail.com>
2015-12-03 15:14:53 +0900

    フォークボタンの上下ずれが気持ち悪いが、プログラム自体の表示を完了

--- application/views/middle.php
+++ application/views/middle.php
@@ -1,20 +1,19 @@
 <h1><?php echo $projectName ?></h1>
-
 <!-- タグ一覧 -->
 <div class="row">
     <div class="col s12">
-<?php
-foreach($tags as $tag){
-    echo '<div class="chip">' .$tag. '</div>';
-}
-?>
+        <?php
+        foreach($tags as $tag){
+            echo '<div class="chip">' .$tag. '</div>';
+        }
+        ?>
     </div>
 </div>
-
-<pre><code class="haxe"><?php echo $program; ?></code></pre>
-
+<div>
+    <a href="">タグ編集</a>
+</div>
 <div class="row description">
-    <div class="col s10 offset-s1">
+    <div class="col s12">
         <div class="card-panel white">
             <span>
                 <?php echo nl2br($description); ?>
@@ -25,5 +24,27 @@ foreach($tags as $tag){
 
 <!-- pvなどのデータ群 -->
 <div>
-
+<ul style="text-align: right;">
+    <li class="middle">pv:<?php echo $pv; ?>,</li>
+    <li class="middle">fork:<?php echo $fork; ?>,</li>
+    <li class="middle">modified:<?php echo $modified;?></li>
+</ul>
 </div>
+
+<hr />
+
+<div class="row">
+    <div class="col s12">
+        <a class="waves-effect waves-light btn col s4 offset-s4" href="">フォーク</a>
+    </div>
+</div>
+
+<pre><code class="haxe"><?php echo $program; ?></code></pre>
+
+<div class="row">
+    <div class="col s12">
+        <a class="waves-effect waves-light btn col s4 offset-s4" href="">フォーク</a>
+    </div>
+</div>
+
+<hr />
