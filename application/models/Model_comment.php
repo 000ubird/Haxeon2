9215ec52398df6e4ee9c47ecc9a6aaf9ba9bedc6
mukko <mukk0.aquarius@gmail.com>
2015-12-03 16:42:45 +0900

    コメントモデルを作成

---
+++ application/models/Model_comment.php
@@ -0,0 +1,20 @@
+<?php
+
+class Model_comment extends CI_Model{
+
+    //コメントを登録する
+    public function registComment($projectID, $comment, $commentedUserID){
+
+    }
+
+    //コメントを削除する
+    public function deleteComment($commentID){
+
+    }
+
+    //コメントを取得する
+    //projectIDに紐づけられた全てのコメントを結果として返す
+    public function getComment($projectID){
+
+    }
+}
