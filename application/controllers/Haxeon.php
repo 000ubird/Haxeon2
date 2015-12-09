7ad259457b910c79582a1d5971dd8695b829d2b6
mukko <mukk0.aquarius@gmail.com>
2015-12-09 14:23:53 +0900

    エラー表示されないように条件を追加

--- application/controllers/Haxeon.php
+++ application/controllers/Haxeon.php
@@ -7,20 +7,22 @@ class Haxeon extends CI_Controller {
 		$this->load->model('Model_project');
 		$ranking_projects = $this->Model_project->getRankingProject();
 		$table_project = array();
-		
+
 		//デイリーランキングテーブルから取得したプロジェクトIDで、プロジェクトテーブルを検索
 		foreach($ranking_projects as $project) {
 			$pro = $this->Model_project->getOneProject($project->proID);
-			
-			//公開プロジェクトのみを配列に追加する
-			if ($pro[0]->isPublic) {
-				$pro[0]->pv = $project->pv;	//デイリーランキングからPV数を取得
-				array_push($table_project,$pro);
-			}
+
+            if($pro) {
+                //公開プロジェクトのみを配列に追加する
+                if ($pro[0]->isPublic) {
+                    $pro[0]->pv = $project->pv;    //デイリーランキングからPV数を取得
+                    array_push($table_project, $pro);
+                }
+            }
 		}
-		
+
 		$data['ranking'] = array('projects'=>$table_project);
-		
+
 		$this->load->view('header');
 		$this->load->view('haxeon',$data);
 		$this->load->view('footer');
