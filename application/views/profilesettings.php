0c668f42cbd9fca0686e3608476cfd6078c51684
Torimura Takanobu <000ubird@gmail.com>
2015-12-16 12:22:54 +0900

    インデントの修正

--- application/views/profilesettings.php
+++ application/views/profilesettings.php
@@ -1,132 +1,112 @@
 <h3>アカウント設定</h3>
 
 <div class="row">
-    <div class="col s12 orange darken-4">
-        <ul class="tabs">
-            <li class="tab col s6"><a class="active" href="#common">情報</a></li>
-            <li class="tab col s6"><a href="#important">重要</a></li>
-        </ul>
-    </div>
+	<div class="col s12 orange darken-4">
+		<ul class="tabs">
+			<li class="tab col s6"><a class="active" href="#common">情報</a></li>
+			<li class="tab col s6"><a href="#important">重要</a></li>
+		</ul>
+	</div>
 </div>
 
-<div id="common" class="col s12">
-    <?php
+<?php
+$uname = "";
+$url = "";
+$message = "";
 
-    $uname = "";
-    $url = "";
-    $message = "";
+foreach($user as $row){
+$uname = $row->userName;
+$url = $row->userURL;
+$message = $row->userProfile;
+}
+echo '<div id="common" class="col s12">';
+	echo form_open('profile/validation_profile/'.$userID);
+	
+	echo '<div class="row">';
+	echo 	'<div class="input-field col s12">';
+	echo 		'<input name="userName" type="text" value="'.$uname.'">';
+	echo 		'<label for="userName">公開ネーム</label>';
+	echo 	'</div>';
+	echo 	form_error('userName');
+	echo '</div>';
 
-    foreach($user as $row){
-        $uname = $row->userName;
-        $url = $row->userURL;
-        $message = $row->userProfile;
-    }
+	echo '<div class="row">';
+	echo 	'<div class="input-field col s12">';
+	echo		'<input name="url" type="text" value="'.$url.'">';
+	echo		'<label for="url">ホームページ</label>';
+	echo	'</div>';
+	echo	form_error('url');
+	echo '</div>';
 
-    echo form_open('profile/validation_profile/'.$userID);
+	echo '<div class="row">';
+	echo 	'<div class="input-field col s12">';
+	echo 		'<textarea name="profile" class="materialize-textarea" maxlength="140">'.$message.'</textarea>';
+	echo 		'<label for="profile">自己紹介</label>';
+	echo 	'</div>';
+	echo 	form_error('profile');
+	echo '</div>';
 
-    echo '<div class="row">';
-    echo '<div class="input-field col s12">';
-    echo '<input name="userName" type="text" value="'.$uname.'">';
-    echo '<label for="userName">公開ネーム</label>';
-    echo '</div>';
+	echo '<div class="row">';
+	echo 	'<button class="btn waves-effect waves-light orange darken-4 col s3 offset-s9" type="submit" name="action">変更';
+	echo 	'<i class="material-icons">open_in_new</i>';
+	echo 	'</button>';
+	echo '</div>';
 
-    echo form_error('userName');
+	echo form_close();
 
-    echo '</div>';
-
-    echo '<div class="row">';
-    echo '    <div class="input-field col s12">';
-    echo '        <input name="url" type="text" value="'.$url.'">';
-    echo '        <label for="url">ホームページ</label>';
-    echo '    </div>';
-
-        echo form_error('url');
-
-    echo '</div>';
-
-    echo '<div class="row">';
-    echo '<div class="input-field col s12">';
-    echo '    <textarea name="profile" class="materialize-textarea" maxlength="140">'.$message.'</textarea>';
-    echo '    <label for="profile">自己紹介</label>';
-    echo '</div>';
-
-    echo form_error('profile');
-
-    echo '</div>';
-
-    echo '<div class="row">';
-    echo '<button class="btn waves-effect waves-light orange darken-4 col s3 offset-s9" type="submit" name="action">変更';
-    echo '<i class="material-icons">open_in_new</i>';
-    echo '</button>';
-    echo '</div>';
-
-
-    echo form_close();
-
-
-    echo '<br />';
-    //echo '<hr />';
-
-
-    echo form_open_multipart('profile/icon_upload/'.$userID);
-    ?>
-<!-- アイコン-->
-    <br />
-    <div class="row">
-        <div class="col s3">
-            <?php
-                $iconurl = "";
-                foreach ($user as $row) {
-                    $iconurl = $row->userIcon;
-                }
-
-            echo '<img src="'.$iconurl.'" width="100" height="100">';
-             ?>
-        </div>
-        <div class="col s9">
-            <form action="#">
-            <div class="file-field input-field">
-                <div class="btn">
-                    <span>アイコン</span>
-                    <input type="file" name="userfile"/>
-                </div>
-                <div class="file-path-wrapper">
-                    <input class="file-path validate" type="text" />
-                </div>
-            </div>
-            <div class="row" align="right">
-            <button class="btn waves-effect" type="submit" value="upload">アップロード
-            </div>
-            </form>
-        </div>
-    </div>
-    </form>
+	echo '<br/><br/>';
+	echo form_open_multipart('profile/icon_upload/'.$userID);
+?>
+	<!-- アイコン-->
+	<div class="row">
+		<div class="col s3">
+			<?php foreach ($user as $row) echo '<img src="'.$row->userIcon.'" width="100" height="100">'; ?>
+		</div>
+		
+		<div class="col s9">
+			<form action="#">
+				<div class="file-field input-field">
+					<div class="btn">
+						<span>アイコン</span>
+						<input type="file" name="userfile"/>
+					</div>
+					
+					<div class="file-path-wrapper">
+						<input class="file-path validate" type="text" />
+					</div>
+				</div>
+				
+				<div class="row" align="right">
+					<button class="btn waves-effect" type="submit" value="upload">アップロード
+				</div>
+			</form>
+		</div>
+	</div>
+	</form>
 </div>
 
 <div id="important" class="col s12">
+	<div class="row">
+		<?php
+		echo '<button class="btn waves-effect waves-light orange darken-4 col s4 offset-s4" value="パスワードを変更する" onClick="location.href=\''. base_url() .'profile/change_pass/'. $userID .'\'">パスワードを変更する';
+		?>
+		<i class="material-icons left">lock_open</i>
+		</button>
+	</div>
 
-    <div class="row">
-    <?php
-    echo '<button class="btn waves-effect waves-light orange darken-4 col s4 offset-s4" value="パスワードを変更する" onClick="location.href=\''. base_url() .'profile/change_pass/'. $userID .'\'">パスワードを変更する';
-    ?>
-    <i class="material-icons left">lock_open</i>
-    </button>
-    </div>
+	<div class="row">
+		<?php
+		echo '<button class="btn waves-effect waves-light orange darken-4 col s4 offset-s4" value="メールアドレスを変更する" onClick="location.href=\''. base_url() .'profile/change_email/'. $userID .'\'">メールアドレスを変更する';
+		?>
+		<i class="material-icons left">email</i>
+		</button>
+	</div>
 
-    <div class="row">
-    <?php
-    echo '<button class="btn waves-effect waves-light orange darken-4 col s4 offset-s4" value="メールアドレスを変更する" onClick="location.href=\''. base_url() .'profile/change_email/'. $userID .'\'">メールアドレスを変更する';
-    ?>
-    <i class="material-icons left">email</i>
-    </button>
-    </div>
-
-    <div class="row">
-    <?php
-	    echo '<button class="btn waves-effect waves-light red col s4 offset-s4" value="アカウントを削除する" onClick="location.href=\''. base_url() .'profile/delete\'">アカウントを削除する';
-    ?>
-    <i class="material-icons left">error_outline</i>
-    </button>
-    </div>
-
+	<div class="row">
+		<?php
+		echo '<button class="btn waves-effect waves-light red col s4 offset-s4" value="アカウントを削除する" onClick="location.href=\''. base_url() .'profile/delete\'">アカウントを削除する';
+		?>
+		<i class="material-icons left">error_outline</i>
+		</button>
+	</div>
 </div>
